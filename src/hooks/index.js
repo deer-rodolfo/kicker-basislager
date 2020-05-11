bff11638e1200d8f4a0293f7e0d23a7f9aeb56ec
deer-rodolfo <rodolfo.borges.dutra@gmail.com>
2020-05-11 16:03:22 +0200

    change playerInterface

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,15 @@
 import { useState, useEffect } from "react";
 import { firebase } from "../firebase";
 
+const playerPoints = (wins, loses) => {
+  return Math.floor(wins * 3 - loses * 2);
+};
+
+const winRatio = (wins, loses) => {
+  const games = wins + loses;
+  return games ? Math.floor((wins / loses) * 100) : 0;
+};
+
 export const usePlayers = () => {
   const [players, setPlayers] = useState([]);
 
@@ -9,17 +18,19 @@ export const usePlayers = () => {
       .firestore()
       .collection("players")
       .get()
-      .then(snapshot => {
-        const allPlayers = snapshot.docs.map(player => ({
+      .then((snapshot) => {
+        const allPlayers = snapshot.docs.map((player) => ({
           id: player.id,
           games: player.data().wins + player.data().loses,
-          ...player.data()
+          winRatio: winRatio(player.data().wins, player.data().loses),
+          points: playerPoints(player.data().wins, player.data().loses),
+          ...player.data(),
         }));
         if (JSON.stringify(allPlayers) !== JSON.stringify(players)) {
           setPlayers(allPlayers);
         }
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, [players]);
 
   return { players, setPlayers };
